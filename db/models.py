e7e126994306a6b3a5f2f2b006ef6ce1f7bf6e21
AlmostVictor <zhovtanetskyj.victor@gmail.com>
2026-03-08 19:30:47 +0200

    test ticket str

--- db/models.py
+++ db/models.py
@@ -94,7 +94,7 @@ class Ticket(models.Model):
 
     def __str__(self) -> str:
         return (
-            f"Ticket: {self.movie_session.movie.title} "
+            f"{self.movie_session.movie.title} "
             f"{self.movie_session.show_time} "
             f"(row: {self.row}, seat: {self.seat})"
         )
